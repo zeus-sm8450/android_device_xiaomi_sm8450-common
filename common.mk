72da562b1320368fa6521d453d4cb8f1473e048f
Mathieu Chartier <mathieuc@google.com>
2023-06-26 19:26:06 +0800

    sm8450-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -26,6 +26,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sm8450-common/sm8450-common-vendor.mk)
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Platform
 TARGET_BOARD_PLATFORM := taro
 
