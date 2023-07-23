deae64cb2c65a0284bcffa34083f54d672327911
SebaUbuntu <seba@sebaubuntu.dev>
2023-07-23 22:19:47 +0100

    sm8450-common: Move to AOSP NXP NFC HAL

--- common.mk
+++ common.mk
@@ -312,8 +312,12 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    libchrome.vendor
+    android.hardware.nfc_snxxx@1.2-service \
+    com.android.nfc_extras \
+    NfcNci \
+    libchrome.vendor \
+    SecureElement \
+    Tag
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
