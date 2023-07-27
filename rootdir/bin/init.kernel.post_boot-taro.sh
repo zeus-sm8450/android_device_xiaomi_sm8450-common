8f99bf7f774f746e7af972babef12fab67eb195a
arnodorian-r <geekroy98@gmail.com>
2023-07-27 16:15:13 +0530

    sm8450-common: bring back foreground boost and top-app cpusets
    ** adjust top-app cpuset to 0-6 cores

--- rootdir/bin/init.kernel.post_boot-taro.sh
+++ rootdir/bin/init.kernel.post_boot-taro.sh
@@ -88,8 +88,10 @@ echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 # cpuset parameters
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
+echo 0-6 > /dev/cpuset/top-app/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 echo 0-6 > /dev/cpuset/foreground/cpus
+echo 4-7 > /dev/cpuset/foreground/boost/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
