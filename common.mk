35e6f320416f8443ac4244a4584416afd5fb1d5c
przekichane <ejbtrd@gmail.com>
2023-11-30 00:08:03 +0530

    sm8450-common: Build android.hardware.drm@1.4.vendor
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- common.mk
+++ common.mk
@@ -181,6 +181,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     libdrm.vendor
 
 # Dumpstate
