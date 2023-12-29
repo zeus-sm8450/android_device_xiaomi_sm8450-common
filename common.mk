a0569feec0455b26f5b042c70941043095d24938
Rasenkai <rasenkai99@gmail.com>
2023-12-29 05:56:11 +0000

    s/overlay-lineage/overlay-pa
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- common.mk
+++ common.mk
@@ -344,7 +344,7 @@ PRODUCT_PACKAGES += \
     WifiResTarget_spf
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-pa \
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
