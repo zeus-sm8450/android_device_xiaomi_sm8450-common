f76abc93735611fca907ce646238a6d052722bd4
Rasenkai <rasenkai99@gmail.com>
2023-12-29 06:07:50 +0000

    Revert "sm8450-common: Build Lineage Health HAL"
    This reverts commit a2726de3a1376e0341dd379edd4f297614490d22.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,9 +126,6 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
-
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
